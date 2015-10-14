9f9c0433f6b6f5c7da6ee89096ebcc8c6523f6b8
sshibani <siawash.shibani@gmail.com>
2015-10-14 11:36:35 +0200

    initial commit

---
+++ DD4TExampleWebsite/Controllers/PageController.cs
@@ -0,0 +1,29 @@
+﻿using DD4T.ContentModel.Contracts.Configuration;
+using DD4T.ContentModel.Contracts.Logging;
+using DD4T.ContentModel.Factories;
+using DD4T.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DD4TExampleWebsite.Controllers
+{
+    public class PageController : TridionControllerBase
+    {
+
+        public PageController(IPageFactory pageFactor, 
+                                IComponentPresentationFactory componentPresentationFactory, 
+                                ILogger logger, 
+                                IDD4TConfiguration configuration) :
+            base(pageFactor, componentPresentationFactory, logger, configuration)
+        {
+
+        }
+        public override ActionResult Page(string url)
+        {
+            return base.Page(url);
+        }
+    }
+}
---
+++ DD4TExampleWebsite/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using DD4T.DI.Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace DD4TExampleWebsite
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        ILifetimeScope BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.UseDD4T();
+            return builder.Build();
+        }
+        protected void Application_Start()
+        {
+            var container = BuildContainer();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
